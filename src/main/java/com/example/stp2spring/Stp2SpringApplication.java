2fab3a5af918a9957a702264fd63992dedb0d34a
Ozsbourn <mibaranin@gmail.com>
2022-06-03 09:42:22 +0500

    s

--- src/main/java/com/example/stp2spring/Stp2SpringApplication.java
+++ src/main/java/com/example/stp2spring/Stp2SpringApplication.java
@@ -1,4 +1,4 @@
-package com.example.stp2_spring;
+package com.example.stp2spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
